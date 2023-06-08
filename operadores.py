e626e022b8aaddee2da1eb8c39083de1069e5ffb
Serigo <s.erickgalindo3@gmail.com>
2023-06-08 14:43:44 -0600

    03 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# Operadores matematicos
+# + - / * %
+# /division flotante
+# // division entera 
+# ** potencia 
+print(24+62)
+print(50-32)
+print(35*2)
+print(10 % 3)
+print(10/3)
+print(10//3)
+print(3**3)
+
+#operadores string
+# + para concatenar 
+# *replicar un String 
+
+print("Hola a"+" todos !")
+print("DSM 3A"* 4)
