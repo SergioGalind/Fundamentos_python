4731ad75c48034edb9f06e7fe9ff7bbedf2f94ad
Serigo <s.erickgalindo3@gmail.com>
2023-06-08 14:33:06 -0600

    02 variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+#python es NO TIPADO
+name = "Sergio"
+age = 27
+is_single = True
+print(name)
+print(age)
+print(is_single)
+
+"""
+Hola soy Erick, tengo 27 años y soltero = true
+"""
+#Utilizando format String
+print("Hola soy {}, tengo {} años y soltero = {}".
+format (name, age, is_single))
+
+print(f"Hola soy {name}, tengo {age} años y soltero = {is_single}")
