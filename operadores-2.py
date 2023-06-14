7ddefebfce97b77d29ba44fdc6ba312a9c52188c
Serigo <s.erickgalindo3@gmail.com>
2023-06-14 12:36:54 -0600

    05 operadores Logicos y Boleanos

---
+++ operadores-2.py
@@ -0,0 +1,24 @@
+#operadores Booleanos 
+print(operadores booleanos)
+# < <= 
+print (10<10)
+print(10<=10)
+#> >=
+print(10>10)
+print(10>=10)
+#==
+print(10==10)
+# !=
+print(10!=10)
+
+#operadores logicos 
+print(operadores logicos)
+# and (True if all true)
+print(True and True)
+print(True and False)
+#or(true if at least one true)
+print (False or False )
+print (False or True)
+# not true 
+print(not True)
+print(not False)
